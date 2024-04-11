5afb29126fd2ebeca80a3ef14e69db0391aa4c4a
rexlypenafloridaii <rpenafloridaii@gmail.com>
2024-04-10 20:46:31 -0500

    Add index and show actions

---
+++ src/components/QuotesIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    photos: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All quotes</h1>
+    <div v-for="quote in quotes" v-bind:key="quote.id">
+      <h2>{{ quote.name }}</h2>
+      <imag v0-bind:src="quote.url" />
+      <p>Width: {{ quote.width }}</p>
+      <p>Height: {{ quote.height }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/QuotesNew.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data: function () {
+    return {
+      newQuoteParams: {},
+    };
+  },
+  methods: {
+    handleSubmit: function () {
+      this.$emit("createQuote", this.newQuoteParams);
+      this.newQuoteParams = {};
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>New Quote</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Name:
+        <input type="text" v-model="newQuoteParams.name" />
+      </div>
+      <div>
+        Url:
+        <input type="text" v-model="newQuoteParams.url" />
+      </div>
+      <div>
+        Width:
+        <input type="text" v-model="newQuoteParams.width" />
+      </div>
+      <div>
+        Height:
+        <input type="text" v-model="newQuoteParams.height" />
+      </div>
+      <button type="submit">Create Peter Quote</button>
+    </form>
+  </div>
+</template>
+
+<style></style>
